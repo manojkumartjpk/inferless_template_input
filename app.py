28b7833271462a350f271a65df86a4823e4c2f53
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2024-04-23 12:45:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import time
 import numpy as np
 import torch
 from transformers import pipeline
@@ -16,6 +17,7 @@ class InferlessPythonModel:
     # e.g. in the below code the output name is generated_txt
     def infer(self, inputs):
         prompt = inputs["prompt"]
+        time.sleep(70)
         pipeline_output = self.generator(prompt, do_sample=True, min_length=20)
         generated_txt = pipeline_output[0]["generated_text"]
         return {"generated_text": generated_txt }
