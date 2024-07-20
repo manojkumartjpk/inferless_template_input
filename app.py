796a37af08dddf6d57f56a48d4acdeb2df737900
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2024-07-20 09:57:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import json
+import os
+
 import numpy as np
 import torch
 from transformers import pipeline
@@ -10,6 +12,11 @@ class InferlessPythonModel:
     # replace ##task_type## and ##huggingface_name## with appropriate values
     def initialize(self):
         self.generator = pipeline("text-generation", model="EleutherAI/gpt-neo-125M",device=0)
+        file_path = "/var/nfs-mount/pvc_testing_oct9/test_apr19.txt"
+        if os.path.exists(file_path):
+            print("File exists.", flush=True)
+        else:
+            print("File does not exist.", flush=True)
 
     # inputs is a dictonary where the keys are input names and values are actual input data
     # e.g. in the below code the input name is prompt 
