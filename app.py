ac66b9893098369b20e4685b5f9f9ecf27a9dbdf
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2025-05-02 12:51:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ class InferlessPythonModel:
     # e.g. in the below code the output name is generated_txt
     def infer(self, inputs):
         prompt = inputs["prompt"]
-        time.sleep(500)
+        import vllm
+        
         pipeline_output = self.generator(prompt, do_sample=True, min_length=20)
         generated_txt = pipeline_output[0]["generated_text"]
         return {"generated_text": generated_txt }
