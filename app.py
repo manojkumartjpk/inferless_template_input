831e9dadd0194dd8e8cf3fe04de98adfbef2a05c
manojkumartjpk <45778460+manojkumartjpk@users.noreply.github.com>
2025-08-13 15:42:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ class InferlessPythonModel:
             current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
             with open(self.file_path, 'a') as file:
                 file.write(f"[{self.pod_id}] hello world {current_datetime}\n")
-            time.sleep(1)  # wait 1 second
+            time.sleep(0.1)
 
         return {"generated_text": "text"}
 
